659ae3084355498d8f98cec91596cf439046e494
akeo-quick <akeo_quick@yahoo.co.jp>
2016-05-27 02:01:31 +0900

    Add files via upload

--- src/model.lisp
+++ src/model.lisp
@@ -1,13 +1,6 @@
 (in-package :cl-user)
 (defpackage game-of-life.model
   (:use :cl)
-  (:import-from :local-time
-                :timestamp-year
-                :timestamp-month
-                :timestamp-day
-                :timestamp-hour
-                :timestamp-minute
-                :timestamp-second)
   (:export :init-board
            :refresh-board
            :update-board
@@ -46,8 +39,8 @@
 
 (defun existp (x y)
   "If a cell at (x y) is alive, this function will return t."
-  (when (in-board-p x y)
-    (aref *board* (to-index x y))))
+  (and (in-board-p x y)
+       (aref *board* (to-index x y))))
 
 (defun to-index (x y)
   "Convert a point in x-y plane to array's index."
@@ -55,13 +48,13 @@
 
 (defun birth (x y)
   "A cell at (x y) will be alive."
-  (when (in-board-p x y)
-    (setf (aref *board* (to-index x y)) t)))
+  (and (in-board-p x y)
+       (setf (aref *board* (to-index x y)) t)))
 
 (defun kill (x y)
   "A cell at (x y) will be dead."
-  (when (in-board-p x y)
-    (setf (aref *board* (to-index x y)) nil)))
+  (and (in-board-p x y)
+       (setf (aref *board* (to-index x y)) nil)))
 
 (defun invert-cell (x y)
   "Invert alive/dead of a cell at (x y)."
@@ -71,24 +64,22 @@
 
 (defun neighbor-locations (x y)
   "Return 8 points of around (x y)."
-  (remove-if-not
-   (lambda (location)
-     (in-board-p (car location) (cdr location)))
-   (list (cons (1- x) (1- y))
-         (cons (1- x) (1+ y))
-         (cons (1- x) y)
-         (cons (1+ x) (1- y))
-         (cons (1+ x) (1+ y))
-         (cons (1+ x) y)
-         (cons x (1- y))
-         (cons x (1+ y)))))
+  (remove-if-not (lambda (location)
+                   (in-board-p (car location) (cdr location)))
+                 (list (cons (1- x) (1- y))
+                       (cons (1- x) (1+ y))
+                       (cons (1- x) y)
+                       (cons (1+ x) (1- y))
+                       (cons (1+ x) (1+ y))
+                       (cons (1+ x) y)
+                       (cons x (1- y))
+                       (cons x (1+ y)))))
 
 (defun count-neighbor (x y)
   "Count the alive cells around (x y)."
-  (count-if
-   (lambda (location)
-     (aref *board* (to-index (car location) (cdr location))))
-   (neighbor-locations x y)))
+  (count-if (lambda (location)
+              (existp (car location) (cdr location)))
+            (neighbor-locations x y)))
 
 (defun next-status (x y)
   "Return next status (alive/dead) of a cell at (x y)."
@@ -117,25 +108,23 @@
 (defun read-board (path)
   "Read initial state of board from file."
   (with-open-file (stream path :direction :input)
-    (let* ((cell-list (read stream t nil)))
+    (let ((cell-list (read stream t nil)))
       (loop :for (x . y) :in cell-list :do
          (birth x y)))))
 
 (defun save-board ()
   "Write current state of board to file. The file will write in save/YYYYMMDDhhmmss.lisp."
-  (let* ((now (local-time:now))
-         (path (format nil "~A/save/~A~A~A~A~A~A.lisp"
-                       (asdf:system-source-directory :game-of-life)
-                       (timestamp-year now)
-                       (timestamp-month now)
-                       (timestamp-day now)
-                       (timestamp-hour now)
-                       (timestamp-minute now)
-                       (timestamp-second now))))
+  (let ((path (format nil "~A/save/~A.lisp"
+                      (asdf:system-source-directory :game-of-life)
+                      (local-time:format-timestring
+                       nil
+                       (local-time:now)
+                       :format '((:YEAR 4) (:MONTH 2) (:DAY 2)
+                                 (:HOUR 2) (:MIN 2) (:SEC 2))))))
     (with-open-file (stream path :direction :output)
       (format stream "~S" (let (out)
                             (dotimes (i *x-range*)
                               (dotimes (j *y-range*)
-                                (when (existp i j)
-                                  (push (cons i j) out))))
+                                (and (existp i j)
+                                     (push (cons i j) out))))
                             out)))))
